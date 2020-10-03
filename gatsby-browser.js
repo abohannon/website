aa66309b65ccc8e7e527e78097b238634df23128
Adam Bohannon <abo46n2@gmail.com>
2020-10-03 16:52:37 -0700

    Updated gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,9 @@
-import React, { useEffect } from "react";
-
 // Activates Google Optimize experiments
 const activateOptimize = () => {
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({ event: "optimize.activate" });
 };
 
-const AppWrapper = ({ element }) => {
-  useEffect(() => {
-    activateOptimize();
-  }, []);
-
-  return element;
+export const onRouteUpdate = () => {
+  activateOptimize();
 };
-export const wrapRootElement = ({ element }) => (
-  <AppWrapper element={element} />
-);
